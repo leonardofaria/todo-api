01e10e72f9fbb2e4e1e3c75cdc699b818c928f4d
Leonardo Faria <leonardofaria@gmail.com>
2016-07-18 19:33:42 -0700

    Add ActiveRecord validations

--- models/list.rb
+++ models/list.rb
@@ -1,3 +1,6 @@
 class List < ActiveRecord::Base
 	has_many :tasks, dependent: :destroy
+
+	validates :name, presence: true
+	validates :color, presence: true, format: /\A#?(?:[A-F0-9]{3}){1,2}\z/i
 end
