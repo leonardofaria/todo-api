75e85f351155b61bfab2532c9cf4c3c5f4c0ee0c
Leonardo Faria <leonardofaria@gmail.com>
2016-07-18 19:36:00 -0700

    Add ActiveRecord method to alter auto increment value

--- config/environments.rb
+++ config/environments.rb
@@ -17,4 +17,19 @@ configure :production, :development do
 		database: db.path[1..-1],
 		encoding: 'utf8'
 	)
+
+	ActiveRecord::Base.class_eval do
+	  def self.reset_autoincrement(options={})
+	    options[:to] ||= 1
+	    case self.connection.adapter_name
+	      when 'MySQL'
+	        self.connection.execute "ALTER TABLE #{self.table_name} AUTO_INCREMENT=#{options[:to]}"
+	      when 'PostgreSQL'
+	        self.connection.execute "ALTER SEQUENCE #{self.table_name}_id_seq RESTART WITH #{options[:to]};"
+	      when 'SQLite'
+	        self.connection.execute "UPDATE sqlite_sequence SET seq=#{options[:to]} WHERE name='#{self.table_name}';"
+	      else
+	    end
+	  end
+	end
 end
